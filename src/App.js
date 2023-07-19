8f91467105f533f3896a638f2ca70c1a8e627215
tyson <83587481+tysonlmao@users.noreply.github.com>
2023-07-19 19:16:54 +1000

    we're getting somewhere now

--- src/App.js
+++ src/App.js
@@ -35,38 +35,22 @@ function formatFirstLogin(firstLogin) {
   return year;
 }
 
-function Footer() {
-  const [commitId, setCommitId] = useState('');
-
-  useEffect(() => {
-    // Fetch the commit ID from the GitHub API
-    const fetchCommitId = async () => {
-      try {
-        const response = await axios.get(
-          'https://api.github.com/repos/tysonlmao/pixelstats/commits'
-        );
-        if (response.data.length > 0) {
-          const latestCommit = response.data[0];
-          setCommitId(latestCommit.sha.substring(0, 7));
-        }
-      } catch (error) {
-        console.log(error);
-      }
-    };
-
-    fetchCommitId();
-  }, []);
-
+function Footer({ commitId }) {
   return (
     <footer>
-      <p>made by <a href="https://tysonlmao.dev">tysonlmao.dev</a> • <a href="https://github.com/tysonlmao/pixelstats">{commitId}</a></p>
+      <p>
+        made by <a href="https://tysonlmao.dev">tysonlmao.dev</a> •{' '}
+        <a href="https://github.com/tysonlmao/pixelstats">{commitId}</a>
+      </p>
     </footer>
   );
 }
 
-function App() {
+export default function App() {
+  const [username, setUsername] = useState("");
   const [uuid, setUuid] = useState("");
   const [stats, setStats] = useState(null);
+  const [commitId, setCommitId] = useState('');
 
   const API_KEY = "8dcd98de-b58f-4f7a-a54f-a42e1084f326";
   const API_URL = `https://api.hypixel.net/player?uuid=${uuid}&key=${API_KEY}`;
@@ -87,11 +71,44 @@ function App() {
     }
   };
 
-  useEffect(() => {
-    if (uuid) {
+  const getUUID = async () => {
+    try {
+      const response = await axios.get(`https://api.mojang.com/users/profiles/minecraft/${encodeURIComponent(username)}`);
+      if (response.data && response.data.id) {
+        setUuid(response.data.id);
+      } else {
+        setUuid("");
+      }
+    } catch (error) {
+      console.log(error);
+      setUuid("");
+    }
+  };
+
+  const handleSearch = () => {
+    if (username) {
+      getUUID();
       getStats();
     }
-  }, [uuid]);
+  };
+
+  const fetchCommitId = async () => {
+    try {
+      const response = await axios.get(
+        'https://api.github.com/repos/tysonlmao/pixelstats/commits'
+      );
+      if (response.data.length > 0) {
+        const latestCommit = response.data[0];
+        setCommitId(latestCommit.sha.substring(0, 7));
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  useEffect(() => {
+    fetchCommitId();
+  }, []);
 
   return (
     <div className="App">
@@ -103,39 +120,42 @@ function App() {
         </div>
       </div>
       <div className="container stats">
-        {!stats ? (
+        {!uuid && (
           <div className="search-form text-end">
             <input
               type="text"
-              value={uuid}
-              onChange={(e) => setUuid(e.target.value)}
-              placeholder="Enter UUID"
+              value={username}
+              onChange={(e) => setUsername(e.target.value)}
+              placeholder="Enter username"
             />
-            <button onClick={getStats} className="btn btn-primary">
+            <button onClick={handleSearch} className="btn btn-primary">
               Get Stats
             </button>
           </div>
-        ) : (
-          <>
-            <div className="row">
-              <div className="col-md">
-                <h2>{stats.player.displayname}</h2>
-                <h3>Joined in {formatFirstLogin(stats.player.firstLogin)}</h3>
-              </div>
-              <div className="col-md text-md-end">
-                <h3>{formatKarma(stats.player.karma)} KARMA</h3>
-                <h3>{stats.player.achievementPoints} AP</h3>
-                {isNaN(stats.player.lastLogin) ? null : (
-                  <h3>{formatLastLogin(stats.player.lastLogin)} last login</h3>
-                )}
-              </div>
-            </div>
-          </>
         )}
+        {uuid && !stats ? (
+          <div className="loading-message">
+            <p>Loading stats...</p>
+          </div>
+        ) : null}
+        {stats ? (
+          <div className="row">
+            <div className="col-md">
+              <h2>{stats.player.displayname}</h2>
+              <h3>Joined in {formatFirstLogin(stats.player.firstLogin)}</h3>
+            </div>
+            <div className="col-md text-md-end">
+              <h3>{formatKarma(stats.player.karma)} KARMA</h3>
+              <h3>{stats.player.achievementPoints} AP</h3>
+              {isNaN(stats.player.lastLogin) ? null : (
+                <h3>{formatLastLogin(stats.player.lastLogin)} last login</h3>
+              )}
+            </div>
+          </div>
+        ) : null}
       </div>
-      <Footer />
+      <Footer commitId={commitId} />
     </div>
   );
 }
 
-export default App;
