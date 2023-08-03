8d306499d0a94d98b776c730befb4b9e3171504a
tyson <83587481+tysonlmao@users.noreply.github.com>
2023-08-03 11:55:48 +1000

    check for updates

--- api/inc/tasks.py
+++ api/inc/tasks.py
@@ -1,6 +1,7 @@
 import os
 import requests
 import json
+import subprocess
 
 def track_players():
     with open('../data/players.json', 'r') as json_file:
@@ -8,3 +9,19 @@ def track_players():
         data_fixed = json.loads(data)
         print(data_fixed)
 track_players()
+
+def check_for_updates():
+    result = subprocess.run(['git', 'rev-parse', 'HEAD'], capture_output=True, text=True)
+    current_commit_id = result.stdout.strip()
+
+    with open('last_commit_id.txt', 'r') as file:
+        last_commit_id = file.read().strip()
+
+    if current_commit_id != last_commit_id:
+        with open('last_commit_id.txt', 'w') as file: 
+            file.write(current_commit_id)
+        
+    subprocess.run(['../deploy.sh'], capture_output=True, text=True)
+
+if __name__ == "__main__":
+    check_for_updates()
