5b9eb203cfe73c491279b0dc19595ec553ae44b7
tyson <83587481+tysonlmao@users.noreply.github.com>
2023-10-14 17:52:32 +1100

    dashboard account switch

--- dashboard.php
+++ dashboard.php
@@ -24,8 +24,8 @@ if ($accountInfo) {
     $altAccount = $accountInfo['alt_account'];
 } else {
     // Account information not found
-    $mainAccount = "N/A";
-    $altAccount = "N/A";
+    $mainAccount = "Unset";
+    $altAccount = "Unset";
 }
 ?>
 
@@ -40,33 +40,112 @@ if ($accountInfo) {
     <link rel="stylesheet" href="./css/style.css">
     <link rel="stylesheet" href="./css/fonts.css">
     <link rel="stylesheet" href="./css/animate.css">
+    <style>
+        /* Hide the tab navigation elements */
+        #accountTabs {
+            display: none;
+        }
+    </style>
     <script src="./js/greetings.js"></script>
 </head>
 
-<body>
+<body class="dashboard">
     <?php include "./templates/header.php" ?>
     <main>
         <div class="container">
-            <div class="d-flex">
+            <div class="d-flex mb-3">
                 <h2 id="greeting"></h2>
-                <h2 class="px-2"><?php echo $_SESSION['userUsername'] ?></h2>
+                <h2 class="px-2" id="accountTitle"><?php echo $_SESSION['userUsername'] ?></h2>
             </div>
+            <?php
+            // echo $mainAccount; // dump it to the screen
 
-            <div class="account-info">
-                <?php
-                echo $mainAccount; // dump it to the screen
-                echo "<script>";
-                echo "let mainAccountValue = '" . $mainAccount . "';";
-                echo "</script>";
-                ?>
+            $apiUrl = "https://api.pixelstats.app/requests?uuid=" . $mainAccount;
+            $ch = curl_init($apiUrl);
 
-                <!-- <h4>Alt Account: <?php // echo $altAccount; 
-                                        ?></h4> -->
+            // curl options
+            curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+            $res = curl_exec($ch);
+            if ($res === false) {
+                echo '<div class="alert alert-danger">cURL Error: ' . curl_error($ch) . '</div>';
+            } else {
+                // if cURL success
+                $data = json_decode($res, true);
+            }
+            ?>
+            <!-- Header with the "Switch Accounts" button -->
+            <header class="box signup-header">
+                <div class="container text-center switcher">
+                    <h2 id="mainAccountTitle"><?php echo $mainAccount ?></h2>
+                    <h3 id="altAccountTitle"><?php echo $altAccount ?></h3>
+                    <button class="btn btn-dark" id="switchButton">
+                        <p>Switch Accounts</p>
+                    </button>
+                </div>
+            </header>
+
+            <div class="tab-content" id="accountTabsContent">
+                <div class="tab-pane fade show active" id="mainAccount" role="tabpanel" aria-labelledby="mainAccount-tab">
+                    <!-- Content for main account -->
+                    <div class="row">
+                        <div class="col-md-4">
+                            <div class="box-no-border">test</div>
+                        </div>
+                        <div class="col-md-8">
+                            <div class="box-no-border">test</div>
+                        </div>
+                    </div>
+                </div>
+                <div class="tab-pane fade" id="altAccount" role="tabpanel" aria-labelledby="altAccount-tab">
+                    <!-- Content for alt account -->
+                    <div class="row">
+                        <div class="col-md-4">
+                            <div class="box-no-border">test</div>
+                        </div>
+                        <div class="col-md-8">
+                            <div class="box-no-border">test</div>
+                        </div>
+                    </div>
+                </div>
             </div>
         </div>
     </main>
-    <script type="module" src="./js/dashboard_view.js"></script>
     <?php include "./templates/footer.php" ?>
+
+    <!-- Include Bootstrap JS for tab functionality -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.min.js"></script>
+
+    <script>
+        // Function to switch the account titles, tabs, and content
+        function switchAccounts() {
+            const mainTitle = document.getElementById("mainAccountTitle");
+            const altTitle = document.getElementById("altAccountTitle");
+            const mainTab = document.getElementById("mainAccount-tab");
+            const altTab = document.getElementById("altAccount-tab");
+            const mainContent = document.getElementById("mainAccount");
+            const altContent = document.getElementById("altAccount");
+
+            // Swap the text content of h2 and h3
+            const tempTitle = mainTitle.textContent;
+            mainTitle.textContent = altTitle.textContent;
+            altTitle.textContent = tempTitle;
+
+            // Toggle active content
+            mainContent.classList.toggle("show");
+            mainContent.classList.toggle("active");
+            altContent.classList.toggle("show");
+            altContent.classList.toggle("active");
+        }
+        // Add event listener to the "Switch Accounts" button
+        const switchButton = document.getElementById("switchButton");
+        switchButton.addEventListener("click", function() {
+            // Call the function to switch account titles, tabs, and content
+            switchAccounts();
+        });
+    </script>
+
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
 </body>
 
 </html>
