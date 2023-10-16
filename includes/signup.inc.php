67a0994721da16cca5f12a87c0bd49e902448414
tyson <83587481+tysonlmao@users.noreply.github.com>
2023-10-16 14:24:28 +1100

    #6 add to user_accounts aswell as users table

--- includes/signup.inc.php
+++ includes/signup.inc.php
@@ -1,33 +1,33 @@
 <?php
 
-// bail early
+// Bail early if the form was not submitted directly
 if (!isset($_POST['register-submit'])) :
-    die("file cannot be directly accessed.");
+    die("File cannot be directly accessed.");
 endif;
 
 require "./connection.inc.php";
 
-// superglobals
+// Superglobals
 $username = $_POST['username'];
 $email = $_POST['email'];
 $password = $_POST['password'];
 $passwordConfirm = $_POST['passwordConfirm'];
 
-// validation
+// Validation
 if (empty($username) || empty($email) || empty($password) || empty($passwordConfirm)) :
-    // catch empty fields
+    // Catch empty fields
     header("Location: ../register.php?error=emptyFields&username=$username&email=$email");
 elseif (!preg_match("/^[a-zA-Z0-9_.-]{3,20}$/", $username)) :
-    // catch bad username
+    // Catch bad username
     header("Location: ../register.php?error=invalidUsername&username=$username&email=$email");
 elseif (!filter_var($email, FILTER_VALIDATE_EMAIL)) :
-    // catch invalid email
+    // Catch invalid email
     header("Location: ../register.php?error=invalidEmail&username=$username&email=$email");
 elseif ($password !== $passwordConfirm) :
-    // catch password mismatch
+    // Catch password mismatch
     header("Location: ../register.php?error=passwordMismatch&username=$username&email=$email");
 else :
-    // all checks passed, fall through to see if username or email is already taken
+    // All checks passed, fall through to see if username or email is already taken
     $sql_check_username = "SELECT username FROM users WHERE username = :username";
     $stmt_check_username = $pdo->prepare($sql_check_username);
     $stmt_check_username->bindParam(':username', $username, PDO::PARAM_STR);
@@ -44,10 +44,7 @@ else :
         header("Location: ../register.php?error=emailTaken&username=$username&email=$email");
     else :
 
-        /**
-         * @todo make signing up also reserve a spot in the user_accounts section
-         */
-        // Username is not taken, proceed with registration
+        // All checks passed, proceed to register the user in the users table
         $sql = "INSERT INTO users (username, email, password) VALUES (:username, :email, :password)";
         $stmt = $pdo->prepare($sql);
         $hashedPassword = password_hash($password, PASSWORD_DEFAULT);
@@ -58,8 +55,15 @@ else :
 
         try {
             $stmt->execute();
+            $newUserId = $pdo->lastInsertId(); // Get the ID of the newly registered user
+
+            // Insert a row into the user_accounts table
+            $sql_user_accounts = "INSERT INTO user_accounts (user_id, main_account, alt_account, cactus_kit) VALUES (:user_id, NULL, NULL, NULL)";
+            $stmt_user_accounts = $pdo->prepare($sql_user_accounts);
+            $stmt_user_accounts->bindParam(':user_id', $newUserId, PDO::PARAM_INT);
+            $stmt_user_accounts->execute();
+
             header("Location: ../register.php?error=success");
-            // header("Location: ../register.php?signup=success");
         } catch (PDOException $e) {
             header("Location: ../register.php?error=sqlerror");
         }
