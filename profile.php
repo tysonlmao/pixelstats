7b5ce23724ea619e7499a4d15a8f90abc0aa6018
tyson <83587481+tysonlmao@users.noreply.github.com>
2023-10-17 21:53:41 +1100

    show player displaynmae in meta description

--- profile.php
+++ profile.php
@@ -29,6 +29,7 @@ if ($res === false) {
 
 <head>
     <meta charset="UTF-8">
+    <meta name="description" content="pixelstats profile | <?php echo $data['player']['displayname']; ?>">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>pixelstats | <?php echo $_GET['player'] ?></title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
@@ -37,7 +38,6 @@ if ($res === false) {
     <link rel="stylesheet" href="./css/animate.css">
     <!-- Add Bootstrap CSS link here -->
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.1/font/bootstrap-icons.min.css">
-
 </head>
 
 <body class="profile content">
