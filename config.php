1837005b5bde6537439d3c936f94bb7b1ffba7aa
tyson <83587481+tysonlmao@users.noreply.github.com>
2023-10-18 15:16:12 +1100

    bug fixes

--- config.php
+++
@@ -1,7 +0,0 @@
-<?php
-// start of pixelstats config
-
-define('RECAPTCHA_SECRET_KEY', '6LeIt6ooAAAAADaVfjluN1H9EQcumeBwQkUaEkOq');
-define('PDO_DBNAME', 'pixelstats');
-define('PDO_DBUSER', 'root');
-define('PDO_DBPASSWORD', 'root');
--- includes/connection.inc.php
+++ includes/connection.inc.php
@@ -1,7 +1,9 @@
 <?php
 
+include "config.php";
+
 try {
-    $pdo = new PDO('mysql:host=localhost;dbname=pixelstats', 'root', 'root');
+    $pdo = new PDO('mysql:host=localhost;dbname=' . PDO_DBNAME, PDO_DBUSER, PDO_DBPASSWORD);
 } catch (PDOException $e) {
     die("Error connecting to the server: $e");
 }
