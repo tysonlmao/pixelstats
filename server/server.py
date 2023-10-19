fed846984e593fa96e48af15534ae1bfd9dc26d9
tyson <83587481+tysonlmao@users.noreply.github.com>
2023-10-19 11:53:45 +1100

    Update server.py

--- server/server.py
+++ server/server.py
@@ -62,6 +62,8 @@ def get_hypixel_data(username):
     }})
 
     return json_data
+
+
 app = Flask(__name__)
 
 @app.route('/update', methods=['POST'])
@@ -83,73 +85,7 @@ def handle_github_webhook():
         print("Error processing GitHub webhook:", str(e))
         return "Failed to process the webhook", 500
 
-
-
-@app.route('/update', methods=['POST'])
-def handle_github_webhook():
-    try:
-        data = request.json
-        ref = data['ref']  # This will contain the branch name, e.g., "refs/heads/main" or "refs/heads/beta"
-
-        if ref == "refs/heads/main":
-            # Handle updates for the production branch
-            os.system("cd /var/www/pixelstats && git stash && git pull")
-
-        elif ref == "refs/heads/beta":
-            # Handle updates for the beta branch
-            os.system("cd /var/www/pixelstats-beta && git stash && git pull")
-
-        return "Webhook received and processed successfully", 200
-    except Exception as e:
-        print("Error processing GitHub webhook:", str(e))
-        return "Failed to process the webhook", 500
-
-
-
-# send requests to this address as a proxy server
-@app.route('/requests', methods=['GET'])
-def main_route():
-    uuid = request.args.get('uuid')
-    data = get_hypixel_data(uuid)
-    return data
-
-    
-
-@app.route('/tea', methods=['GET'])
-def teapot(): 
-    return {"status": "I\'m a teapot"}, 418
-
-@app.route('/', methods=['GET'])
-def why():
-    quotes = [
-    "Fail fast, fail cheaply",
-    "Become lazy",
-    "Read the fucking manual",
-    "Target the low hanging fruit",
-    "Be part of the solution",
-    "Do the simple things",
-    "Start simple, get complex",
-    "Don't ice an uncooked cake",
-    "Less haste, more speed"
-  ]
-    return {"message": random.choice(quotes)};
-
-@app.route('/api/players', methods=['GET'])
-def get_players():
-    try:
-        with open('players.json', 'r') as json_file:
-            data = json_file.read()
-            data_fixed = json.loads(data)
-            pretty_json_string = json.dumps(data_fixed, indent=4)
-        return jsonify(json.loads(pretty_json_string))
-    except FileNotFoundError:
-        return jsonify({"error": "players.json not found"}), 404
-
-def updates_interval(interval):
-    while True:
-        check_for_updates()
-        time.sleep(interval)
-        
+# Rest of your Flask routes and logic here...
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=8073)
@@ -161,4 +97,4 @@ if __name__ == '__main__':
 
     while True:
         # Your main application logic or server code goes here
-        time.sleep(1) 
+        time.sleep(1)
